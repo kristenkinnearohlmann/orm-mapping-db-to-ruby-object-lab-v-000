31ed67d18a803346e5f7e4d2490464e2f44a7b08
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-05 02:03:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     student = self.new
-    student.id = row[0]
+    student.id = 1
   end
 
   def self.all
